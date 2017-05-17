a634a6b563d95bd132ba6186205cac8b8649da83
Eric van Zanten <eric.vanzanten@gmail.com>
2017-05-17 09:44:41 -0500

    Add image_to_form constraint

---
+++ alembic/versions/3b5bf4bf1e3_make_image_to_form_constraint.py
@@ -0,0 +1,31 @@
+"""Make image_to_form constraint
+
+Revision ID: 3b5bf4bf1e3
+Revises: df8fcdd868
+Create Date: 2017-05-17 09:40:02.196959
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3b5bf4bf1e3'
+down_revision = 'df8fcdd868'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.execute(''' 
+        ALTER TABLE image_task_assignment
+        DROP CONSTRAINT IF EXISTS image_to_form
+    ''')
+    op.execute(''' 
+        ALTER TABLE image_task_assignment
+        ADD CONSTRAINT image_to_form UNIQUE (image_id, form_id)
+    ''')
+
+
+def downgrade():
+    pass
